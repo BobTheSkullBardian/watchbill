55024ab0f76af9e034cc8e1224590b2e52b3cb75
Stephen Hunsaker <hansenzhongwenxidexuesheng@gmail.com>
2022-08-19 13:23:35 +0000

    Fixed Allowed Hosts

--- wbc/settings.py
+++ wbc/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '8000-csa63watchbil-watchbill-llv54bzw0fj.ws-us62.gitpod.io',
     'localhost'
-    'watchbill.herokuapp.com
+    'watchbill.herokuapp.com'
     ]
 # ALLOWED_HOSTS = ['SERVER-IP', 'localhost', '127.0.0.1']
 
