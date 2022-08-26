f737933bc5dc19ac66a308b7a77bf6d53ed0a495
Stephen Hunsaker <hansenzhongwenxidexuesheng@gmail.com>
2022-08-26 13:02:51 +0000

    2 sailors added to tracker

--- wbc/settings.py
+++ wbc/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 # DEBUG = False
 
 ALLOWED_HOSTS = [
-    '8000-csa63watchbil-watchbill-ppb6ylwmo6p.ws-us63.gitpod.io',
+    '8000-csa63watchbil-watchbill-yzs93ptwz03.ws-us63.gitpod.io',
     'localhost',
     'watchbill.herokuapp.com',
     '34.232.247.167',
