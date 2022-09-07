00fe1c56262861f64ac9565d9e0cdac224644db9
Stephen Hunsaker <hansenzhongwenxidexuesheng@gmail.com>
2022-09-07 11:40:04 +0000

    watch input

--- wbc/settings.py
+++ wbc/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 # DEBUG = False
 
 ALLOWED_HOSTS = [
-    '8000-csa63watchbil-watchbill-9qw4gn3vc3h.ws-us63.gitpod.io',
+    '8000-csa63watchbil-watchbill-i0dol5ui7pi.ws-us64.gitpod.io',
     'localhost',
     'watchbill.herokuapp.com',
     '34.232.247.167',
