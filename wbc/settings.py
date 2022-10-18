d6399a735ef64fb49e9774aa4e4822f13adb6db7
Stephen Hunsaker <hansenzhongwenxidexuesheng@gmail.com>
2022-10-18 13:18:44 +0000

    Watch ACKS Nov

--- wbc/settings.py
+++ wbc/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 # DEBUG = False
 
 ALLOWED_HOSTS = [
-    '8000-csa63watchbil-watchbill-uyfps7j2c5d.ws-us71.gitpod.io',
+    '8000-csa63watchbil-watchbill-fwkzifwxii2.ws-us71.gitpod.io',
     'localhost',
     'watchbill.herokuapp.com',
     '34.232.247.167',
