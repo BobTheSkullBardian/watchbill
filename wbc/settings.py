3b7a26b8c1fbaed1e4db414b891376d485c3e0f7
Nick Lewis <ncklws93@gmail.com>
2022-11-03 17:18:42 +0000

    Dec watchbill updates

--- wbc/settings.py
+++ wbc/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 # DEBUG = False
 
 ALLOWED_HOSTS = [
-    '8000-csa63watchbil-watchbill-amrfo9v2ebc.ws-us73.gitpod.io',
+    '8000-csa63watchbil-watchbill-k9x2bk4qhxs.ws-us73.gitpod.io',
     'localhost',
     'watchbill.herokuapp.com',
     '34.232.247.167',
