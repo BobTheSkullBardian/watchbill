fbf2f13ff6babf480b5a66c29cbf171d82948d04
Stephen Hunsaker <hansenzhongwenxidexuesheng@gmail.com>
2022-11-09 14:32:35 +0000

    Sailors Qual'd

--- wbc/settings.py
+++ wbc/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 # DEBUG = False
 
 ALLOWED_HOSTS = [
-    '8000-csa63watchbil-watchbill-2gvflvzr4eo.ws-us75.gitpod.io',
+    '8000-csa63watchbil-watchbill-z0hy3sncllv.ws-us75.gitpod.io',
     'localhost',
     'watchbill.herokuapp.com',
     '34.232.247.167',
